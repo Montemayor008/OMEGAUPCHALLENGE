397b5394c5cc41c661a52062d976086275ef6fe8
Melissa Montemayor Riojas <montemayor008@gmail.com>
2019-09-02 14:29:58 -0500

    Update xAplusB.java

--- src/xBasicProgramming/xAplusB.java
+++ src/xBasicProgramming/xAplusB.java
@@ -6,11 +6,14 @@ public class xAplusB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner (System.in);
+		// Input is "Integer 1" and "Integer 2" to sum
+		// Output is the sum of the integers
+		
+		Scanner scan = new Scanner (System.in); 
 		int xInput = scan.nextInt();     //Scans first input
 		int xInputTwo = scan.nextInt();  //Scans second input
 		System.out.print(xInput + xInputTwo);  // Adds inputs and prints result
-		// When program runs, please write two integers to sum in the console
+	
 	}
 
 }
