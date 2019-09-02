ac3a98dfd1cb051a2545a0fa7cd559d1937fa4c0
Melissa Montemayor Riojas <montemayor008@gmail.com>
2019-09-02 14:09:42 -0500

    xAplusB Challenge
    Basic Programming : Addition

---
+++ src/xBasicProgramming/xAplusB.java
@@ -0,0 +1,16 @@
+package xBasicProgramming;
+
+import java.util.Scanner;
+
+public class xAplusB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner (System.in);
+		int xInput = scan.nextInt();     //Scans first input
+		int xInputTwo = scan.nextInt();  //Scans second input
+		System.out.print(xInput + xInputTwo);  // Adds inputs and prints result
+		// When program runs, please write two integers to sum in the console
+	}
+
+}
